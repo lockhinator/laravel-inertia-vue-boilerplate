2a763701f47cdaf52e92680012a582abb9ea0b93
Lockhinator <cameronl.lockhart@gmail.com>
2022-08-01 16:32:10 -0700

    feat: 🎸 inertia, vue3, auth added
    * feat: 🎸 inertia, vue3, auth added
    
    * feat: 🎸 additional docker changes
    
    * feat: 🎸 remove yarn.lock because windows/linux issues
    
    * fix: 🐛 updated so that node builds its own node_modules
    
    * feat: 🎸 node now builds image without deps
    
    * fix: 🐛 fixed php-cs not running correctly
    
    * feat: 🎸 dockerfiles now use image to build
    
    * fix: 🐛 tests updated
    
    * fix: 🐛 updated phpunit.xml
    
    * fix: 🐛 .env added to the test mounts
    
    * fix: 🐛 fix docker compose test
    
    * fix: 🐛 messed up the test compose again
    
    * fix: 🐛 mount local directory
    
    * fix: 🐛 run yarn
    
    * fix: 🐛 build assets before tests
    
    * fix: 🐛 turn redis on for tests
    
    * fix: 🐛 made tests work
    
    * fix: 🐛 removed app_env from docker-compose
    
    * fix: 🐛 reduce test coverage to 85%

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    ssr: {
+        noExternal: ['@inertiajs/server'],
+    },
 });
