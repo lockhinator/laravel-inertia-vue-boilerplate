5235acb4b703f951c040f9689db1406f696789d2
Cameron <cameronl.lockhart@gmail.com>
2022-08-01 14:01:19 -0700

    fix: 🐛 fixed php-cs not running correctly

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
+                return array_merge((new Ziggy())->toArray(), [
                     'location' => $request->url(),
                 ]);
             },
